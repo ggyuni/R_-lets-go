c75cbf3d53f256564c4d93559d2b3c88ed901e35
ggyuni <108729915+ggyuni@users.noreply.github.com>
2022-07-07 16:45:50 +0900

    Add files via upload

---
+++ 2-1.R
@@ -0,0 +1,16 @@
+log(10)+5 ## 로그함수이며 값은 7.302585로 도출됨
+
+
+sqrt(25) ## 제곱근이며 값은 5로 도출됨
+
+
+max(5,3,2) ## 최댓값으로 값은 5로 도출됨
+
+
+#ggplot2 패키지 설치
+install.packages('ggpolt2')
+
+
+#gglot2 불러오기
+library(ggplot2)
+
