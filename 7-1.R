75d7052d6aecadd1ada3964f401a594ab856ffc0
ggyuni <108729915+ggyuni@users.noreply.github.com>
2022-07-19 15:30:24 +0900

    Update 7-1.R

--- 7-1.R
+++ 7-1.R
@@ -9,3 +9,5 @@ person[[1]] ## 리스트의 첫 번째 값을 출력함 "TOM"이 출력됨
 person$name ## 리스트에서 값의 이름이 name인 값을 출력함 "TOM"이 출력됨
 
 person[[4]] ## 리스트의 네 번째 값을 출력함 ("balling" "tennis"  "ski")이 출력됨 
+
+person[1] ## [[]]일때에는 $name이 붙지만 []일때는 붙지 않음
