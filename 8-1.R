a97f7cc266835e108a7c0e94bc7bc485f48f51d3
ggyuni <108729915+ggyuni@users.noreply.github.com>
2022-07-20 22:47:54 +0900

    Add files via upload

---
+++ 8-1.R
@@ -0,0 +1,7 @@
+city <- c("Seoul", "Tokyo","Washington") ## 문자로 이루어진 벡터
+
+rank <- c(1,3,2) ## 숫자로 이루어진 벡터
+
+city.info <- data.frame(city, rank) ## 데이터프레임 생성
+
+city.info ## city.info의 내용 출력,순서는 서울 ,워싱턴, 도쿄 순으로 순위가 매겨짐
