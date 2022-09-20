0ef6d6b078d87e23ab5fc72f430b6a2a2bb7c0b9
ggyuni <108729915+ggyuni@users.noreply.github.com>
2022-09-20 14:11:09 +0900

    Add files via upload

---
+++ pg. 연산자.py
@@ -0,0 +1,7 @@
+#2215813 한동균 연산자 프로그램
+n1 = eval(input("수1 =>"))
+n2 = eval(input("수2 =>"))
+print("%d / %d = %f" %(n1, n2, n1/n2))
+print("%d // %d = %d" %(n1, n2, n1//n2))
+print("%d / %d 의 나머지= %d" %(n1, n2, n1%n2))
+print(n1**n2)
