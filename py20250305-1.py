31abf78728f5c2d2234be06efca4fdac0d486879
ggyuni <108729915+ggyuni@users.noreply.github.com>
2025-03-07 14:52:59 +0900

    eee
    eee

---
+++ py20250305-1.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    print(i+1,"번째 학생 점수를 입력 바람!!!")
+    a=int(input("첫 번째 점수?"))
+    b=int(input("두 번째 점수?"))
+    c=a+b
+    print("국어=",a)
+    print("수학=",b)
+    print("합계:",c)
