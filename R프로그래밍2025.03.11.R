b8bae57b2ec73760b083b371d355bb6a44660885
ggyuni <108729915+ggyuni@users.noreply.github.com>
2025-03-11 11:42:33 +0900

    Add files via upload

---
+++ R프로그래밍2025.03.11.R
@@ -0,0 +1,24 @@
+#run= CTRL + ENTER
+7+4
+#2*3
+log(10)+5
+sqrt(25)
+max(5,3,2)
+10*5*(1/2) #이거도*
+10*10*3.14 #왠지 중요함
+#패키지는 작업중인 컴퓨터의 특정폴더에 저장되어 있어야 로딩가능
+library(ggplot2) # 중요!
+install.packages('ggplot2') #중요!
+#help("sort")
+#상자= 변수, 사과= 데이터
+a<-10
+total<-5050
+total
+print(total)
+cat('합계:',total) #cat=붙다는뜻
+a<-10
+b<-20
+c<-a+b
+print(a)
+print(b)
+print(c)
